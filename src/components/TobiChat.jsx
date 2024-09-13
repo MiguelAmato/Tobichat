0fb5232033638c3a2095c70988aa189596f8a5ab
Miguel Amato <miamato@ucm.es>
2024-09-13 13:57:13 +0200

    Cambiado el estilo a uno más estilizado, cambiada la estructuración del proyecto

--- src/components/TobiChat.jsx
+++ src/components/TobiChat.jsx
@@ -2,7 +2,17 @@ import React, { useState } from 'react'
 import ThemeToggle from './ThemeToggle'
 import MessageList from './MessageList'
 import MessageInput from './MessageInput'
-import { API_KEY } from './Utils'
+import { API_KEY } from '../Utils'
+
+
+/*
+	TobiChat.jsx
+	
+	Componente del chat principal, aqui se produce la llamada a la API de Hugging Face y se 
+	le envia a los componentes de los mensajes la informacion de la llamada a la API para 
+	mostrarlos en pantalla
+
+*/
 
 function TobiChat() {
   const [messages, setMessages] = useState([])
@@ -32,13 +42,18 @@ function TobiChat() {
 		return (result[0].generated_text);
 	};
 
+	// sendMessageToAPI: 
 	const sendMessageToAPI = async (message) => {
 		return query(message);
 	}
   
 
   return (
-    <div className="chat-container">
+	/*
+	chat-container: Componente que contiene todos los elementos del chat, tanto header, lista de mensajes y el input
+	chat-header: Contiene el titulo del chat y el boton del cambio de modo claro a modo oscuro 
+	*/
+    <div className="chat-container">  
       <header className="chat-header">
         <h1>Tobichat</h1>
         <ThemeToggle />
