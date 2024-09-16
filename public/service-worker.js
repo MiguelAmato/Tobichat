6721591ee11e9f9c19e88f3f6a62ca1343a936f9
Miguel Amato <miamato@ucm.es>
2024-09-16 17:53:57 +0200

    Probando a ver si funcionan las notificaciones

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,56 @@
+const CACHE_NAME = 'tobichat-cache-v1';
+const urlsToCache = [
+    '/',
+    '/Tobichat/index.html',
+    '/Tobichat/TobiCon.png',        
+    '/Tobichat/src/styles/chat.css', 
+    '/Tobichat/src/main.jsx',        
+    '/Tobichat/src/components/MessageInput.jsx',
+    '/Tobichat/src/components/MessageList.jsx',
+    '/Tobichat/src/components/ThemeContext.jsx',
+    '/Tobichat/src/components/ThemeToggle.jsx',
+    '/Tobichat/src/components/TobiChat.jsx',
+    '/Tobichat/src/App.jsx'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Archivos en caché');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response; 
+                }
+                return fetch(event.request); 
+            })
+    );
+});
+
+
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('push', (event) => {
 	const options = {
 	  body: event.data.text(),
