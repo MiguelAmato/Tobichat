06ade52d51065067881ecdfe7075a72be47a6ec4
Miguel Amato <miamato@ucm.es>
2024-09-16 19:57:36 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,24 @@
-
-  self.addEventListener('notificationclick', function(event) {
+/*
+	Código que maneja el evento de pulsar la notificacion push en el service worker
+	verifica si estás en la ventana del chat, si no lo estas te abre el chat
+*/
+self.addEventListener('notificationclick', function(event) {
 	event.notification.close(); 
-  
+
 	event.waitUntil(
-	  clients.matchAll({
+		clients.matchAll({
 		type: 'window',
 		includeUncontrolled: true 
-	  }).then(function(clientList) {
+		}).then(function(clientList) {
 		for (let i = 0; i < clientList.length; i++) {
-		  const client = clientList[i];
-		  if (client.url ===  'https://miguelamato.github.io/Tobichat/' && 'focus' in client) {
+			const client = clientList[i];
+			if (client.url ===  'https://miguelamato.github.io/Tobichat/' && 'focus' in client) {
 			return client.focus();
-		  }
+			}
 		}
 		if (clients.openWindow) {
-		  return clients.openWindow( 'https://miguelamato.github.io/Tobichat/');
+			return clients.openWindow( 'https://miguelamato.github.io/Tobichat/');
 		}
-	  })
+		})
 	);
-  });
+});
